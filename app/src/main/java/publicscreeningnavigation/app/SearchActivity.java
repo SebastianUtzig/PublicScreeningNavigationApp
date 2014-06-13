6f1478181a043d426e8cfc3c105a5b62285de1e3
Johannes <johanneshartmann90@gmx.de>
2014-06-13 02:54:31 +0200

    search activty looks good so far
    needs result activty to pop up
    singleton store or somethig like that needed now

--- app/src/main/java/publicscreeningnavigation/app/SearchActivity.java
+++ app/src/main/java/publicscreeningnavigation/app/SearchActivity.java
@@ -2,16 +2,31 @@ package publicscreeningnavigation.app;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.RadioButton;
+import android.widget.TextView;
 
 public class SearchActivity extends ActionBarActivity {
 
+    private RadioButton nameRadio;
+    private RadioButton tagRadio;
+    private RadioButton locationRadio;
+    private TextView searchView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_seach);
 
+        nameRadio = (RadioButton)findViewById(R.id.nameRadio);
+        tagRadio = (RadioButton)findViewById(R.id.tagRadio);
+        locationRadio = (RadioButton)findViewById(R.id.locationRadio);
+        searchView = (TextView)findViewById(R.id.editText);
     }
 
 
@@ -35,4 +50,34 @@ public class SearchActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    public void locationRadioHit(View view){
+        locationRadio.setChecked(true);
+        tagRadio.setChecked(false);
+        nameRadio.setChecked(false);
+    }
+
+    public void tagRadioHit(View view){
+        Log.d("Button hit","tag radio");
+        locationRadio.setChecked(false);
+        tagRadio.setChecked(true);
+        nameRadio.setChecked(false);
+
+    }
+
+    public void nameRadioHit(View view){
+        Log.d("Button hit","name radio");
+        locationRadio.setChecked(false);
+        tagRadio.setChecked(false);
+        nameRadio.setChecked(true);
+    }
+
+    public void initSearch(View view){
+        CharSequence searchString = searchView.getText();
+        if (searchString.length() < 0){
+            //do search
+        }
+        else {
+            //toast: no search string!
+        }
+    }
 }
