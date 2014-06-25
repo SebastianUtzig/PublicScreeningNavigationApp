c90069c54b0a665e5e78655d9d6b4c50dd6de0ee
Johannes <johanneshartmann90@gmx.de>
2014-06-25 11:02:10 +0200

    Thousands of bullshits
    Fuck you, AS!

--- app/src/main/java/publicscreeningnavigation/app/MainActivity.java
+++ app/src/main/java/publicscreeningnavigation/app/MainActivity.java
@@ -2,7 +2,7 @@ package publicscreeningnavigation.app;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
